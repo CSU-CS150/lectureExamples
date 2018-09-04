92b86c8c6028addc35d1f419ce825e1638726742
Albert Lionelle <Albert.Lionelle@colostate.edu>
2018-09-03 21:19:31 -0600

    Added readme

---
+++ src/MethodLecture.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+/**
+ * Methods used in methods lecture 5
+ *
+ * @author Albert Lionelle <br>
+ *         lionelle@colostate.edu <br>
+ *         Computer Science Department <br>
+ *         Colorado State University
+ * @version 1.0
+ */
+public class MethodLecture {
+
+    public static String getName() {
+        return "Melody Pond";
+    }
+
+
+    public static void main(String args[]) {
+
+        System.out.println("Hello, my name is " + getName());
+
+
+    }
+
+
+}
